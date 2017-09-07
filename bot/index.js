543dd4ed6d47b52703e841f79e21ffa8194f8699
Rui Carmo <rui.carmo@gmail.com>
2017-09-07 18:54:08 +0100

    Another test

--- bot/index.js
+++ bot/index.js
@@ -30,9 +30,34 @@ if (process.env.FUNCTIONS_EXTENSION_VERSION) {
     // If we are inside Azure Functions, export the standard handler.
     module.exports = function (context, req) {
         context.log("Handling request", req);
-        // Try to patch body and pass request to Bot Framework
+        var _status = null;
+        var _body = null;
+        var _respond = function (status, body) {
+            context.res = {
+                status: status || 200,
+                body: body || ''
+            };
+            context.done();
+        };
+        // wrap response
+        var res = {
+            send: function (status, body) {
+                _respond(status, body);
+            },
+            status: function (status) {
+                _status = status;
+            },
+            write: function (body) {
+                _body = body;
+            },
+            end: function () {
+                _respond(_status, _body);
+            }
+        };
+        // Pass request to Bot Framework
         req.body = req['body'];
-        listener(req, context.res);
+        context.log("Passing body", req.body);
+        listener(req, res);
     }
 } else {
     // Local server for testing
